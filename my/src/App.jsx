4e2d32d2e0fd147c0ad00e573c350ee888a44db7
keerthana290506 <keerthana290506.com>
2025-01-21 15:07:57 +0530

    Done creating login page

---
+++ my/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { Login } from './Component/Login'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Routes>
+      <Route path="/login" element={<Login/>}/>
+    </Routes>
+
+    </>
+  )
+}
+
+export default App
