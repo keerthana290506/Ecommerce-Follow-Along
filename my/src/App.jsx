bba58d6d91502c1c68f219d0b4493a6b8e815204
keerthana290506 <keerthana290506.com>
2025-02-15 11:25:20 +0530

    front

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -1,19 +1,32 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import {Navbar} from './Component/Navbar'
+import { Route, Routes ,Router } from 'react-router-dom';
 import { Login } from './Component/Login';
 import { Signup } from './Component/Signup';
 import { Home } from './Component/Home';
-import { Productform } from './Component/Productform';
+import { ProductForm } from './Component/Productform';
+import { Productcardseller } from './Component/productcardSeller';
+
+
 
 function App() {
   return (
     <>
+  
+    <Router>
+      <Navbar/>
+    
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path='/home' element={<Home/>}/>
-        <Route path='/productform' element={<Productform/>}/>
-      </Routes>
+        <Route path='/productform' element={<ProductForm/>}/>
+        <Route path='/my-product' element={<Productcardseller/>}/>
+             </Routes>
+       
+        </Router>
+     
+             
     </>
   );  
 }
